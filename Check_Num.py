7c3d847cbc8d6521bf2e42741df40046cbc1fade
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-14 20:58:51 +0500

    Check Int

---
+++ Check_Num.py
@@ -0,0 +1,10 @@
+# Taking input from the user
+num = float(input("Enter a number: "))
+
+# Checking if the number is positive, negative, or zero
+if num > 0:
+    print(num, "is positive.")
+elif num < 0:
+    print(num, "is negative.")
+else:
+    print("The number is zero.")
