1fbabd04322bbf575fe27c6d9cdc1efbbb0e5299
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-14 20:48:27 +0500

    User input Addition

---
+++ Add_Two_Num_User_Input.py
@@ -0,0 +1,11 @@
+# Taking input from the user for the first number
+num1 = float(input("Enter the first number: "))
+
+# Taking input from the user for the second number
+num2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Printing the sum
+print("The sum of", num1, "and", num2, "is:", sum)
