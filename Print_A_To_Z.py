fcd2261bc891b45d52e33c88652dd12d09164015
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-16 19:17:21 +0500

    A to Z

---
+++ Print_A_To_Z.py
@@ -0,0 +1,7 @@
+letter = ord('A')  # Get the ASCII value of 'A'
+
+while letter <= ord('Z'):  # Loop until 'Z'
+    print(chr(letter), end=' ')  # Print the character corresponding to the ASCII value
+    letter += 1  # Move to the next character
+
+# Output will be: A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
