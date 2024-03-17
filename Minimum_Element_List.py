204da0be4b363fe130f7b80cc0df3808bba7f2c1
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-17 19:49:39 +0500

    Minimum

---
+++ Minimum_Element_List.py
@@ -0,0 +1,8 @@
+# Define a list
+my_list = [10, 5, 20, 15, 30]
+
+# Find the smallest element in the list
+smallest_element = min(my_list)
+
+# Print the smallest element
+print("Smallest element in the list:", smallest_element)
