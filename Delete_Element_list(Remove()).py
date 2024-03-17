e2dbaf1235e65e6f772cb9106ae4733303ff7635
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-17 20:01:28 +0500

    Delete

---
+++ Delete_Element_list(Remove()).py
@@ -0,0 +1,8 @@
+# Define a list
+my_list = [1, 2, 3, 4, 5]
+
+# Remove a specific value from the list (e.g., the value 3)
+my_list.remove(3)
+
+# Print the list after deletion
+print("List after deleting the value 3:", my_list)
