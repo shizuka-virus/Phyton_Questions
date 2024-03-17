8c46cc8e877504927c8604106eba40cbda4e1613
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-17 20:03:09 +0500

    Delete

---
+++ Delete_Element_List(pop()).py
@@ -0,0 +1,9 @@
+# Define a list
+my_list = [1, 2, 3, 4, 5]
+
+# Remove and return the element at a specific index (e.g., index 2)
+deleted_element = my_list.pop(2)
+
+# Print the deleted element and the list after deletion
+print("Deleted element:", deleted_element)
+print("List after deleting element at index 2:", my_list)
