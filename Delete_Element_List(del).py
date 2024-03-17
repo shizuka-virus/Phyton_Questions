f1ec4fb13a17712d60760e4569e49de211e0f777
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-17 19:55:11 +0500

    Del

---
+++ Delete_Element_List(del).py
@@ -0,0 +1,8 @@
+# Define a list
+my_list = [1, 2, 3, 4, 5]
+
+# Delete the element at a specific index (e.g., index 2)
+del my_list[2]
+
+# Print the list after deletion
+print("List after deleting element at index 2:", my_list)
