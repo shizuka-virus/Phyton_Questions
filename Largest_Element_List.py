13941fe82e1e68c51d9d84ffbbd0da539941746d
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-17 19:48:37 +0500

    Largest

---
+++ Largest_Element_List.py
@@ -0,0 +1,8 @@
+# Define a list
+my_list = [10, 5, 20, 15, 30]
+
+# Find the largest element in the list
+largest_element = max(my_list)
+
+# Print the largest element
+print("Largest element in the list:", largest_element)
