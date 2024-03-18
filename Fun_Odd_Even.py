9cdd8dda1af25d657b2107ae7ebf84849ec57a7f
shizuka-virus <muhammadfarooq1085@gmail.com>
2024-03-18 20:12:46 +0500

    Odd even

---
+++ Fun_Odd_Even.py
@@ -0,0 +1,9 @@
+def check_odd_even(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Example usage:
+print(check_odd_even(5))  # Output: Odd
+print(check_odd_even(8))  # Output: Even
